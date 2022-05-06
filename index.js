14c07c01be8a7fcc07ed154ef1e87a33869a9a25
Julie <jatkin80@ford.com>
2022-05-06 12:04:10 -0600

    build and burn 2

--- index.js
+++
@@ -1,5 +0,0 @@
-function subtract ( a, b )
-{
-  return b - a;
-}
-module.exports = subtract;
--- index.test.js
+++
@@ -1,5 +0,0 @@
-const subtract = require( './index' )
-test( 'subtracts 2-1 to equal 1', () =>
-{
-  expect( subtract( 1, 2 ) ).toBe( 1 );
-} );
